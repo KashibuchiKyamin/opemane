b76905746fed6bd1dced8ec64a1229ade06ad36a
KashibuchiKyamin <y.mrkm.0722@gmail.com>
2023-03-08 23:23:23 +0900

    パッケージを変更

--- src/main/java/com/kashibuchikyamin/opemane/config/properties/AccessTokenApiConfig.java
+++ src/main/java/com/kashibuchikyamin/opemane/config/properties/AccessTokenApiConfig.java
@@ -1,4 +1,4 @@
-package com.kashibuchikyamin.opemane.config.constant;
+package com.kashibuchikyamin.opemane.config.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
